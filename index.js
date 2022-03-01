a35159efaba629ea50c1804786345858e026514b
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-03-01 18:53:05 +0500

    mock data are added

--- index.js
+++ index.js
@@ -4,11 +4,41 @@ const Joi = require('joi');
 const app = express();
 app.use(express.json());
 
+let categories = [
+  {
+    id: 1,
+    name: "Dasturlash",
+  }, 
+  {
+    id: 2,
+    name: "DevOps",
+  }, 
+  {
+    id: 3,
+    name: "Tarmoq administratorligi",
+  }, 
+  {
+    id: 4,
+    name: "Axborot Xavfsizligi",
+  }, 
+  {
+    id: 5,
+    name: "Ma'lumotlar ombori",
+  },
+];
+
+app.get('/api/categories', (req, res) => {
+  res.status(200).send();
+});
+
+
+function userDataSanitizator(userData) {
+  return userData.trim();
+}
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`${port} is running`);
-}
-  
+  }
 );
