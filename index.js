e9e1945a21e3e65e087ad1cd61096c8a0fd302fe
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-03-25 15:26:47 +0500

    WIP

--- index.js
+++ index.js
@@ -1,112 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const Joi = require('joi');
-
-const app = express();
-app.use(express.json());
-
-let categories = [
-  {
-    id: 1,
-    name: "Dasturlash",
-  }, 
-  {
-    id: 2,
-    name: "DevOps",
-  }, 
-  {
-    id: 3,
-    name: "Tarmoq administratorligi",
-  }, 
-  {
-    id: 4,
-    name: "Axborot Xavfsizligi",
-  }, 
-  {
-    id: 5,
-    name: "Ma'lumotlar ombori",
-  },
-];
-
-app.get('/api/categories', (req, res) => {
-  res.status(200).send(categories);
-});
-
-app.get('/api/categories/:id', (req, res) => {
-  const requestedCategoryId = parseInt(req.params.id);
-  const requestedCategory = categories.find(category => category.id === requestedCategoryId);
-  res.status(200).send(requestedCategory);
-});
-
-app.post('/api/categories', (req, res) => {
-  const { error } = categoryValidator(req.body);
-  if( error ) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  const newCategory = {
-    id: categories.length + 1,
-    name: req.body.name,
-  };
-
-  categories.push(newCategory);
-
-  return res.status(201).send(categories[categories.length-1]);
-});
-
-app.put('/api/categories/:id', (req, res) => {
-  const { error } = categoryValidator(req.body);
-
-  if( error ) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  if(userIdParamValidator(req.params.id) === "invalid") {
-    return res.status(400).send("Ko'rsatilgan ID ga ega bo'lgan categoriya topilmadi!");
-  }
-
-  const updatingCategoryIndex = categories.findIndex(category => category.id === parseInt(req.params.id));
-  const updatedCategory = {
-    id: updatingCategoryIndex+1,
-    name: userDataSanitizator(req.body.name),
-  }
-  categories.splice(updatingCategoryIndex, 1, updatedCategory);
-  return res.status(200).send(categories[updatingCategoryIndex]);
-});
-
-app.delete('/api/categories/:id', (req, res) => {
-  if(userIdParamValidator(req.params.id) === "invalid") {
-    return res.status(400).send("Ko'rsatilgan ID ga ega bo'lgan categoriya topilmadi!");
-  }
-  const updatingCategoryIndex = categories.findIndex(category => category.id === parseInt(req.params.id));
-  categories.splice(updatingCategoryIndex, 1, );
-  return res.status(200).send(categories);
-});
-
-function categoryValidator(validatingObject) {
-  const categoryScheme = Joi.object({
-    name: Joi.string().required().min(3),
-  });
-
-  return categoryScheme.validate(validatingObject);
-}
-
-function userDataSanitizator(userData) {
-  return userData.trim();
-}
-
-function userIdParamValidator(id) {
-  const updatingCategoryId = parseInt(id);
-
-  return (updatingCategoryId < 1 || updatingCategoryId > categories.length) ? "invalid" : "valid"
-}
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`${port} is running`);
-  }
-=======
 const express = require('express');
 const Joi = require('joi');
 const categories = require('./routes/categories');
@@ -115,12 +6,9 @@ const app = express();
 app.use(express.json());
 app.use('/api/categories', categories);
 
-
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`${port} is running`);
   }
->>>>>>> 7767f1d690a8c80dae36a0dbe9f28d2c296a80b5
 );
