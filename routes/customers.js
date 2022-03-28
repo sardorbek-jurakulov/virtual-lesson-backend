981cbb042d8c535adb27c61f2760d7a5ce5f1ca1
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-03-28 22:15:11 +0500

    WIP

--- routes/customers.js
+++ routes/customers.js
@@ -3,13 +3,49 @@ const config = require('config');
 const Joi = require('joi');
 const router = express.Router();
 
-route.get('/', (req, res) => {
+const customersSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    minlength: 2,
+    maxlength: 30,
+    trim: true
+  },
+  isVip: {
+    type: Boolean,
+    default: false
+  },
+  phone: {
+    type: String,
+    required: true,
+    minlength: 7,
+    maxlength: 11,
+    trim: true
+  }
+});
 
+const Customers = mongoose.model("Customers", customersSchema);
+
+route.get('/', async (req, res) => {
+  const customers = await Customers.find().sort({name: 1});
+  res.status(200).send(customers);
 });
 
 route.post('/', (req, res) => {
-
+  const { error } = customerValidator(req.body);
+  if(error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  
 });
+
+function customerValidator(validatingObject) {
+  const categorySchema = Joi.object({
+    name: Joi.string().required().min(2),
+    isVip: Joi.boolean(),
+    phone: Joi.string().required().min(7).max(11)
+  });
+}
 
 
 module.exports = router;
